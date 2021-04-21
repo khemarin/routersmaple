31718013d3146e6757265bf60ce1b349f9c07d3f
Khemarin Loch <khemarin.loch@rakuten.com>
2021-04-21 15:04:16 +0900

    Initial Commit

---
+++ RouterSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RouterSample
+//
+//  Created by Loch, Khemarin | Cameron | ECMPD on 2021/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
